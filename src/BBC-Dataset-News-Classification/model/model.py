ed6046529cf5b5d7ac211e170b52c5460bae52fa
Ali <newaaa4@gmail.com>
2019-04-12 14:54:35 +0300

    push

--- src/BBC-Dataset-News-Classification/model/model.py
+++ src/BBC-Dataset-News-Classification/model/model.py
@@ -20,26 +20,42 @@ vect = TfidfVectorizer(stop_words='english', min_df=2)
 X = vect.fit_transform(x)
 Y = np.array(y)
 
-print(X.shape)
 
 print("no of features extracted:", X.shape[1])
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, Y, test_size=0.30, random_state=42)
 
+# print()
+# print(X_train[1, :])
+# print()
+
+with open("../csv/train.txt", "w") as f:
+    for c in X_train:
+        for i in c:
+            f.write(str(i))
+
+# pd.DataFrame(X_train).to_csv("../csv/train.csv")
+# pd.DataFrame(X_test).to_csv("../csv/test.csv")
+# pd.DataFrame(y_train).to_csv("../csv/train_lables.csv")
+# pd.DataFrame(y_test).to_csv("../csv/test_lables.csv")
+
+# print()
+# print(X_train)
+# print()
+
 print("train size:", X_train.shape)
 print("test size:", X_test.shape)
 
 model = SVC(gamma='scale', kernel='rbf', probability=True)
 
-
-# model.fit(X_train, y_train)
+model.fit(X_train, y_train)
 
 # with open("model.bin", "wb") as modelFile:
 #     pickle.dump(model, modelFile)
 
-with open("model.bin", "rb") as modelFile:
-    model = pickle.load(modelFile)
+# with open("model.bin", "rb") as modelFile:
+#     model = pickle.load(modelFile)
 
 y_pred = model.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
@@ -49,20 +65,3 @@ print("\nAccuracy:  ", round(acc*100, 2), "%", sep="")
 
 with open("tfidf.bin", "wb") as featuresFile:
     pickle.dump(vect, featuresFile)
-
-# news = """It's what representing their country is all about -- walking out in New Zealand's All Blacks jersey, facing their opposition, and delivering a spine-tingling, hair-raising Haka before the whistle blows for kick-off.
-
-# The sights and sounds of the Haka -- feet stomping, fists pumping, vocal chords straining -- are deeply entrenched within New Zealand culture.
-# "For me, the Haka is a symbol of who we are and where we come from," former All Blacks captain Richie McCaw told CNN in 2015.
-# "This is who we are. Obviously it comes from a Maori background but I think it also resonates with all Kiwis """
-# x = ' '.join([word for word in stringClean(news).split()])
-# X = vect.transform([x])
-# # print(model.predict(X))
-# lables = ['business', 'entertainment', 'politics', 'sport', 'tech']
-
-# prob = model.predict_proba(X)
-# for value, lable in zip(prob[0], lables):
-#     print(lable, "\t\t", round(value*100, 2), "%", sep="")
-print("\n\n\n")
-print(vect.stop_words_)
-print("\n\n")
